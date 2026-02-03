76c0a72834dd11a5fb1bd5703a46f797e220f5ef
Arijeet Roy <blakelabs57@gmail.com>
2026-02-03 23:29:32 +0530

    Basics covered

---
+++ integers.py
@@ -0,0 +1,13 @@
+add = 4+4
+subtract = 10-2
+multiply = 4*2
+divide = 80/10
+
+print(add)
+print(subtract)
+print(multiply)
+print(divide)
+
+
+number = 57
+print("Hey my lucky number is " + str(number))
--- simplMessage.py
+++ simplMessage.py
@@ -1,3 +1,33 @@
 text = "how are you?"
 text = "I am fine"
 print(text)
+
+#name = "brad pitt"
+#print(name.title())
+
+#below is the example of string concatenation
+first_name = "brad"
+last_name = "pitt"
+full_name = first_name + " " + last_name
+
+print(full_name.title())
+
+#personal message
+name = "henry"
+print("How are you shittin today " + name.title() + " ?")
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+famous_person = "richard feynman"
+quote = ( famous_person.title() + ' once said, "Learn in the most \nundisciplined manner possible"')
+print(quote)
+
+
+
+stripper_payload = '" \t\nuser name "'
+print(stripper_payload)
+print(stripper_payload.lstrip())
+print(stripper_payload.rstrip())
+print(stripper_payload.strip())
